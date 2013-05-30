42eb1358f96df20a343bd87a981032e357d9886f
j <j@j-x220.(none)>
2013-05-30 15:16:40 +0800

    j

--- python/jhmath.py
+++ python/jhmath.py
@@ -30,12 +30,63 @@ def linefit(x, y):
 
 
 def linefit_same_step(y):
+    '''linefit for just one List'''
     x = range(1, len(y)+1)
     return linefit(x, y)
 
 
+def _default_weight_func(offset):
+    if offset <= -3 or offset >= 3:
+        return 0
+    elif offset == 0:
+        return 2
+    return abs(1/abs(offset))
+
+def smooth(data, weight_func=_default_weight_func):
+    '''
+    smooth data according to weights geted by function weight_func:
+    def weight_func(offset):
+        if offset <= -3 or offset >= 3:
+            return 0
+        elif offset == 0:
+            return 2
+        return abs(1/abs(offset))
+    then, smooth_data[i] = (data[i]*2 + data[i+1]*1 + data[i-1]*1 + \
+        data[i-2]*1/2 + data[1+2]*1/2)/(2+1+1+1/2+1/2)
+    data is a List of float or int
+    return a new List of smoothed data
+    '''
+    weight_dic = {}
+    for off in range(len(data)):
+        weight = weight_func(off)
+        if weight != 0:
+            weight_dic[off] = weight
+        weight = weight_func(-off)
+        if weight != 0:
+            weight_dic[-off] = weight
+
+    ret_data = []
+    data_length = len(data)
+    for i in range(data_length):
+        d = data[i]
+        data_sum = 0
+        weight_sum = 0
+        for off in weight_dic:
+            if (i+off) >= 0 and (i+off) < data_length:
+                data_sum += data[i+off] * weight_dic[off]
+                weight_sum += weight_dic[off]
+        if weight_sum == 0:
+            ret_data.append(0)
+        else:
+            ret_data.append(data_sum/weight_sum)
+
+    return ret_data
+
+
 def test():
-    print linefit_same_step([1,2,3,4])
+    # print linefit_same_step([1,2,3,4])
+    data = range(20)
+    print smooth(data)
 
 
 if __name__ == '__main__':
