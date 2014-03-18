0410910cf1e716999003d8c6216d2dd2d592d5c9
jh <jh@ubuntu.(none)>
2014-03-18 10:24:15 +0800

    jh

--- python/jhdb.py
+++ python/jhdb.py
@@ -8,15 +8,19 @@ import traceback
 class Jhdb:
     def __init__(self, host, port, user, passwd, db):
         try:
-            conn = MySQLdb.connect(host=host, port=int(port), user=user, \
+            self.conn = MySQLdb.connect(host=host, port=int(port), user=user, \
                                       passwd=passwd, db=db)
-            conn.autocommit(True)
-            conn.set_character_set('utf8')
-            self.cursor = conn.cursor()
+            self.conn.autocommit(True)
+            self.conn.set_character_set('utf8')
+            self.cursor = self.conn.cursor()
         except Exception, e:
-            print "Jhdb connectiong error, %s" % str(e)
+            print("Jhdb connectiong error, %s" % str(e))
             traceback.print_exc()
 
+
+    def close(self):
+        self.cursor.close()
+        self.conn.close()
 
     def select(self, sql):
         '''
@@ -26,7 +30,7 @@ class Jhdb:
         try:
             self.cursor.execute(sql)
         except Exception, e:
-            print "Jhdb error %s, sql = %s" % (str(e), sql)
+            print("Jhdb error %s, sql = %s" % (str(e), sql))
             return ret
         cols = [desc[0] for desc in self.cursor.description]
         for d in self.cursor.fetchall():
@@ -42,7 +46,7 @@ class Jhdb:
             self.cursor.execute(sql)
             return True
         except Exception, e:
-            print "Jhdb error %s, sql = %s" % (str(e), sql)
+            print("Jhdb error %s, sql = %s" % (str(e), sql))
             return False
 
     def insert(self, sql):
@@ -51,15 +55,40 @@ class Jhdb:
     def delete(self, sql):
         return self.update(sql)
 
+    def _parse_conditions(self, conditions):
+        ''' conditions is a dict of {condition_filed:condition_value} '''
+        return ' AND '.join(
+                ["`%s` = '%s'" % (k,v) for k,v in conditions.items()])
+
+    def delete_by_field(self, table, field, v):
+        sql = "DELETE FROM %s WHERE `%s` = '%s'" % (table, field, v)
+        return self.delete(sql)
+
+    def update_field(self, table, condition_k, condition_v, field, v):
+        sql = "UPDATE %s SET `%s` = '%s' WHERE `%s` = '%s'"
+        sql = sql % (table, field, v, condition_k, condition_v)
+        return self.update(sql)
+
+    def get_by_conditions(self, table, conditions, orderby=None, limit=None):
+        sql = "SELECT * FROM %s WHERE %s"
+        sql = sql % (table, self._parse_conditions(conditions))
+        if orderby is not None:
+            sql = "%s ORDER BY `%s`" % (sql, orderby)
+        if limit is not None:
+            sql = "%s LIMIT %s" % (sql, limit)
+        print(sql)
+        return self.select(sql)
+
+
+def test_parse_conditions(conditions):
+    ''' conditions is a dict of {condition_filed:condition_value} '''
+    return ' AND '.join(
+            ["`%s` = '%s'" % (k,v) for k,v in conditions.items()])
+
 
 def test():
-    db = Jhdb(host="10.20.174.102", \
-              port="3306", \
-              user="root", \
-              passwd="123", \
-              db="aliddin"
-              )
-    print db.select("SELECT * FROM uedpub")
+    conditions = {'id':123, 'f1':'asdfasef', 'f4':'asfies'}
+    print(test_parse_conditions(conditions))
 
 
 if __name__ == "__main__":
---
+++ python/jhhttp.py
@@ -0,0 +1,131 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# jhhttp, a simple API tool, support RESTful APIs
+import simplejson
+import urllib
+import urllib2
+import traceback
+
+class RequestWithMethod(urllib2.Request):
+    def __init__(self, url, method, data=None, headers={},\
+        origin_req_host=None, unverifiable=False):
+        self._method = method
+        urllib2.Request.__init__(self, url, data, headers, origin_req_host,
+                unverifiable)
+
+    def get_method(self):
+        if self._method:
+            return self._method
+        else:
+            return urllib2.Request.get_method(self)
+
+
+class JhhttpError(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+
+def _restful_headers(headers=None):
+    if headers is None:
+        headers = {}
+        headers['Content-Type'] = 'application/json'
+    else:
+        headers['Content-Type'] = 'application/json'
+    return headers
+
+def _restful_data(data):
+    if type(data) is not str:
+        data = simplejson.dumps(data)
+    return data
+
+def _prepare_data(data):
+    if type(data) is str or data is None:
+        return data
+    try:
+        data = urllib.urlencode(data)
+    except Exception, e:
+        traceback.print_exc()
+        raise JhhttpError(e)
+    return data
+
+def _open(url, method, data=None, headers=None):
+    if headers is None:
+        headers = {}
+    try:
+        req = RequestWithMethod(url, method, data, headers)
+        response = urllib2.urlopen(req)
+    except Exception, e:
+        traceback.print_exc()
+        raise JhhttpError(e)
+    return response
+
+def _return(response, response_type):
+    try:
+        content = response.read()
+    except Exception, e:
+        traceback.print_exc()
+        raise JhhttpError(e)
+    if not content:
+        return None
+    if response_type is 'json':
+        try:
+            ret = simplejson.loads(content)
+        except Exception, e:
+            traceback.print_exc()
+            raise JhhttpError(e)
+    else:
+        ret = content
+    return ret
+
+def _check(url, data, headers):
+    if headers is not None and type(headers) is not dict:
+        raise JhhttpError('headers is not a dict')
+
+def _do_http(url, data=None, headers=None, method='GET', response_type='text'):
+    try:
+        _check(url, data, headers)
+        f = _open(url, method, data, headers)
+        ret = _return(f, response_type)
+    except JhhttpError, e:
+        print('JhhttpError, url = %s' % url)
+        ret = None
+    return ret
+
+def rest_put(url, data=None, headers=None):
+    headers = _restful_headers(headers)
+    data = _restful_data(data)
+    return _do_http(url, data, headers, 'PUT', 'json')
+
+def put(url, data=None, headers=None):
+    data = _prepare_data(data)
+    return _do_http(url, data, headers, 'PUT', 'text')
+
+def rest_delete(url, data=None, headers=None):
+    data = _restful_data(data)
+    headers = _restful_headers(headers)
+    return _do_http(url, data, headers, 'DELETE', 'json')
+
+def delete(url, data=None, headers=None):
+    data = _prepare_data(data)
+    return _do_http(url, data, headers, 'DELETE', 'text')
+
+def rest_post(url, data=None, headers=None):
+    data = _restful_data(data)
+    headers = _restful_headers(headers)
+    return _do_http(url, data, headers, 'POST', 'json')
+
+def post(url, data=None, headers=None):
+    data = _prepare_data(data)
+    return _do_http(url, data, headers, 'POST', 'text')
+
+def rest_get(url, data=None, headers=None):
+    data = None
+    headers = _restful_headers(headers)
+    return _do_http(url, data, headers, 'GET', 'json')
+
+def get(url, data=None, headers=None):
+    data = None
+    print headers
+    return _do_http(url, data, headers, 'GET', 'text')
