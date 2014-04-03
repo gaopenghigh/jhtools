6f6f9e15a0b4b12beaa370b12bd31da7a7f1e619
jh <jh@ubuntu.(none)>
2014-04-03 14:35:03 +0800

    jh add jhdb_connector JhdbPool

--- python/jhdb_connector.py
+++ python/jhdb_connector.py
@@ -11,32 +11,10 @@ class JhdbError(Exception):
     def __str__(self):
         return repr(self.value)
 
-
-class Jhdb:
-    def __init__(self, host, port, user, password, database):
-        configs = {
-                'host': host,
-                'port': port,
-                'user': user,
-                'password': password,
-                'database': database,
-                'autocommit': True,
-                'charset': 'utf8',
-                }
-        try:
-            self.cnx = mysql.connector.connect(**configs)
-        except mysql.connector.Error as err:
-            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-                raise JhdbError("Access denied")
-            elif err.errno == errorcode.ER_BAD_DB_ERROR:
-                raise JhdbError("Database %s not exist" % database)
-            else:
-                raise(err)
-        self.cursor = self.cnx.cursor()
-
-    def close(self):
-        self.cursor.close()
-        self.cnx.close()
+class JhdbExcutor:
+    ''' a helper class for JhdbPool and Jhdb, do the real MySQL work '''
+    def __init__(self, cursor):
+        self.cursor = cursor
 
     def _execute(self, sql):
         try:
@@ -88,22 +66,100 @@ class Jhdb:
         return self.select(sql)
 
 
-def test():
-    testdb = {
-       'host': '127.0.0.1',
-       'port': 3306,
-       'user': 'root',
-       'password': '123156',
-       'database': 'jop',
-       }
-    db = Jhdb(**testdb)
-    rets = db.select('select * from clusters')
-    print(rets)
-    for r in rets:
-        print r['varz']
-    print(db.get_by_conditions('clusters', {'varz':'test:test'}))
+class JhdbPool:
+    ''' Do querys using a limited number of connections '''
+    def __init__(self, host, port, user, password, database, pool_size,
+            pool_name):
+        configs = {
+                'host': host,
+                'port': port,
+                'user': user,
+                'password': password,
+                'database': database,
+                'pool_name': pool_name,
+                'pool_size': pool_size,
+                'autocommit': True,
+                'charset': 'utf8',
+                }
+        self.cnxpool = mysql.connector.pooling.MySQLConnectionPool(**configs)
+
+    def pool_method(func):
+        '''
+        We want to do all the work by limited connections,
+        so we use a while True try to:
+            1. get available connection
+            2. get cursor do the SQL query job
+            3. return(close) the connection
+        This is busy wait, may be not good?
+        '''
+        def _wrapper(self, *args, **kwds):
+            while True:
+                try:
+                    cnx = self.cnxpool.get_connection()
+                    break
+                except mysql.connector.errors.PoolError:
+                    continue
+            excutor = JhdbExcutor(cnx.cursor())
+            ret = getattr(excutor, func.__name__)(*args, **kwds)
+            cnx.cursor().close()
+            cnx.close()
+            return ret
+        return _wrapper
 
 
-if __name__ == "__main__":
-    test()
+    @pool_method
+    def select(self, sql):
+        pass
+
+    @pool_method
+    def update(self, sql):
+        pass
+
+    @pool_method
+    def insert(self, sql):
+        pass
+
+    @pool_method
+    def delete(self, sql):
+        pass
+
+    @pool_method
+    def delete_by_field(self, table, field, v):
+        pass
+
+    @pool_method
+    def update_field(self, table, condition_k, condition_v, field, v):
+        pass
+
+    @pool_method
+    def get_by_conditions(self, table, conditions, orderby=None, limit=None):
+        pass
+
+
+class Jhdb(JhdbExcutor):
+    def __init__(self, host, port, user, password, database):
+        configs = {
+                'host': host,
+                'port': port,
+                'user': user,
+                'password': password,
+                'database': database,
+                'autocommit': True,
+                'charset': 'utf8',
+                }
+        try:
+            self.cnx = mysql.connector.connect(**configs)
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                raise JhdbError("Access denied")
+            elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                raise JhdbError("Database %s not exist" % database)
+            else:
+                raise(err)
+        self.cursor = self.cnx.cursor()
+        JhdbExcutor.__init__(self, self.cursor)
+
+    def close(self):
+        self.cursor.close()
+        self.cnx.close()
 
---
+++ python/jhdb_connector_test.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Author: gaopenghigh<gaopenghigh@gmail.com>
+import time
+import threading
+import jhdb_connector as jhdb
+
+def test_Jhdb():
+    testdb = {
+       'host': '127.0.0.1',
+       'port': 3306,
+       'user': 'root',
+       'password': '123156',
+       'database': 'jop',
+       }
+    db = jhdb.Jhdb(**testdb)
+    rets = db.select('select * from clusters')
+    print(rets)
+    for r in rets:
+        print r['varz']
+    print(db.get_by_conditions('clusters', {'varz':'test:test'}))
+
+def query_func(dbpool, name):
+    ret = dbpool.get_by_conditions('clusters', {'varz':'test:test'})
+    print('%s, resul = %s, sleep 5s' % (name, ret))
+    time.sleep(5)
+
+
+def test_JhdbPool():
+    testdb = {
+       'host': '127.0.0.1',
+       'port': 3306,
+       'user': 'root',
+       'password': '123156',
+       'database': 'jop',
+       'pool_name': 'mypool',
+       'pool_size': 1,
+       }
+    dbpool = jhdb.JhdbPool(**testdb)
+    for i in range(5):
+        name = 'thread-' + str(i)
+        threading.Thread(target=query_func, args=(dbpool,name),
+                name=name).start()
+
+if __name__ == '__main__':
+    test_Jhdb()
+    test_JhdbPool()
+
+
+
